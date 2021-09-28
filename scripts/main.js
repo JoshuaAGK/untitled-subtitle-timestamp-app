617406affebf5313271087decc84cde8485ce790
Josh Kelley <josh@greenwoodcampbell.com>
2021-09-29 00:14:41 +0100

    Design change

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,33 @@
 document.getElementById("inputbox").addEventListener('input', changeEvent)
 
+var searchResults = 0;
+var foundFromContent = [];
+
 function changeEvent() {
-    document.getElementById("sublist").innerHTML = null;
+    [...document.querySelectorAll('section[role="grid"] div ul')].forEach(div => {
+        div.innerHTML = "";
+    })
+    searchResults = 0;
 
     var searchtext = document.getElementById("inputbox").value.toLowerCase();
 
-    if (searchtext != "") {
+    if (searchtext != "" && searchtext.length >= 3) {
         for (var j = 0; j < ENTRIES.length; j++) {
             for (var i = 0; i < ENTRIES[j].data.length; i++) {
                 var subtext = ENTRIES[j].data[i].text;
+                if (subtext.toLowerCase().includes(searchtext)) {
+                    if (foundFromContent.indexOf(ENTRIES[j].name) == -1) {
+                        foundFromContent.push(ENTRIES[j].name);
+                        var newSection = document.createElement("div");
+                        var newSectionList = document.createElement("ul");
+                        newSection.setAttribute("content", ENTRIES[j].name);
+                        newSection.appendChild(newSectionList);
+                        var newSectionTitle = document.createElement("h2");
+                        newSectionTitle.innerHTML = ENTRIES[j].name;
+                        newSection.appendChild(newSectionTitle);
+                        document.querySelectorAll('section[role="grid"]')[0].appendChild(newSection);
+                    }
 
-                
-
-                if (subtext.toLowerCase().includes(searchtext) && searchtext.length >= 3) {
                     var sub = document.createElement("li");
                     var substringStartIndex = subtext.toLowerCase().indexOf(searchtext);
                     var substringEndIndex = searchtext.length + substringStartIndex;
@@ -31,10 +46,19 @@ function changeEvent() {
                     minutes = (hours * 60) + minutes;
                     
                     sub.innerHTML = beginningText + markText + endingText + "<br/>" + "<i>(" + ENTRIES[j].name + " " + minutes + ":" + seconds + ")</i>";
-                    document.getElementById("sublist").appendChild(sub);
+                    document.querySelectorAll('div[content="' + ENTRIES[j].name + '"] ul')[0].appendChild(sub);
 
+                    
+
+                    searchResults++;
                 }
             }
         }
     }
+
+    if (searchtext.length >= 3) {
+        document.getElementById("results-num").innerHTML = searchResults.toLocaleString() + " results";
+    } else {
+        document.getElementById("results-num").innerHTML = "";
+    }    
 }
